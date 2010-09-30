9f01a8be86070e8457c7f9d040e241cd87c998eb
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-09-30 20:35:33 +0000

    initialize save-the-network game board

--- harbinPegSTN/src/harbinPegSTN/SaveTheNetworkModel.java
+++ harbinPegSTN/src/harbinPegSTN/SaveTheNetworkModel.java
@@ -2,7 +2,17 @@ package harbinPegSTN;
 
 public class SaveTheNetworkModel extends Model{
 
+	
 	public SaveTheNetworkModel(){
 		super();
+		for(int i=1;i<=6;i++)
+			super.setPegAt(Peg.NONE, i);
+		setPegAt(Peg.BLACK,7);
+		setPegAt(Peg.BLACK,8);
+		setPegAt(Peg.BLACK,12);
+		setPegAt(Peg.BLACK,13);
+		for(int i=14;i<=33;i++)
+			super.setPegAt(Peg.BLACK, i);
+			
 	}
 }
