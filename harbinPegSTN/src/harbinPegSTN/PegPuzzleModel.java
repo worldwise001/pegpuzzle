bcf9429bc466dda24789fc26876c43137972f681
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-09-30 20:21:15 +0000

    Initialize board

--- harbinPegSTN/src/harbinPegSTN/PegPuzzleModel.java
+++ harbinPegSTN/src/harbinPegSTN/PegPuzzleModel.java
@@ -3,9 +3,14 @@ package harbinPegSTN;
 public class PegPuzzleModel extends Model{
 
 	public static final int CENTER_LOC = 17;
-	
+	/*
+	 * Initializing board
+	 */
 	public PegPuzzleModel(){
 		super();
-		
+		for(int i=1;i<33;i++){
+			super.setPegAt(Peg.NORMAL, i);
+		}
+		setPegAt(Peg.BLANK,CENTER_LOC);
 	}
 }
