8414c54499864f9a852a52f0cfebc34c26ff4f07
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-09-30 22:04:56 +0000

    Finish checkMove and makeMove methods for both model

--- harbinPegSTN/src/harbinPegSTN/PegPuzzleModel.java
+++ harbinPegSTN/src/harbinPegSTN/PegPuzzleModel.java
@@ -1,5 +1,7 @@
 package harbinPegSTN;
 
+import java.awt.Point;
+
 public class PegPuzzleModel extends Model{
 
 	public static final int CENTER_LOC = 17;
@@ -12,5 +14,77 @@ public class PegPuzzleModel extends Model{
 			super.setPegAt(Peg.NORMAL, i);
 		}
 		setPegAt(Peg.NONE,CENTER_LOC);
+	}
+	public boolean checkMove(Point fPt,Point sPt){
+		int fLoc=this.pointToPegID(fPt.x, fPt.y);
+		int sLoc=this.pointToPegID(sPt.x, sPt.y);
+		return checkMove(fLoc,sLoc);
+	}
+	public boolean makeMove(Point fPt,Point sPt){
+		int fLoc=this.pointToPegID(fPt.x, fPt.y);
+		int sLoc=this.pointToPegID(sPt.x, sPt.y);
+		return makeMove(fLoc,sLoc);
+	}
+	public boolean checkMove(int firstLoc,int secLoc){
+		if(firstLoc<1||firstLoc>33)
+			return false;
+		if(secLoc<1||secLoc>33)
+			return false;
+		Point mid=new Point();
+		int midLoc=0;
+		Point fPt=pegIDToPoint(firstLoc);
+		Point sPt=pegIDToPoint(secLoc);
+		if(Math.abs(fPt.x-sPt.x)==2 && fPt.y==sPt.y)
+			{
+				mid.y=fPt.y;
+				if(fPt.x>sPt.x)
+						mid.x=fPt.x-1;
+				else
+					mid.x=fPt.x+1;
+				midLoc=super.pointToPegID(mid.x, mid.y);  
+				return (isPegAtLocation(firstLoc)&&isPegAtLocation(midLoc));
+			}
+		if(Math.abs(fPt.y-sPt.y)==2 && fPt.x==sPt.x)
+		{
+			mid.x=fPt.x;
+			if(fPt.y>sPt.y)
+					mid.y=fPt.y-1;
+			else
+				mid.y=fPt.y+1;
+			midLoc=super.pointToPegID(mid.x, mid.y);  
+			return (isPegAtLocation(firstLoc)&&isPegAtLocation(midLoc));
+		}
+		return false;
+	}
+	
+	public boolean makeMove(int firstLoc,int secLoc){
+		if(!checkMove(firstLoc,secLoc))
+			return false;
+		Point mid=new Point();
+		
+		Point fPt=pegIDToPoint(firstLoc);
+		Point sPt=pegIDToPoint(secLoc);
+		if(Math.abs(fPt.x-sPt.x)==2 && fPt.y==sPt.y)
+			{
+				mid.y=fPt.y;
+				if(fPt.x>sPt.x)
+						mid.x=fPt.x-1;
+				else
+					mid.x=fPt.x+1;
+				
+			}
+		if(Math.abs(fPt.y-sPt.y)==2 && fPt.x==sPt.x)
+		{
+			mid.x=fPt.x;
+			if(fPt.y>sPt.y)
+					mid.y=fPt.y-1;
+			else
+				mid.y=fPt.y+1; 
+			
+		}
+		pegs[fPt.x][fPt.y]=Peg.BLANK;
+		pegs[mid.x][mid.y]=Peg.BLANK;
+		pegs[sPt.x][sPt.y]=Peg.NORMAL;
+		return true;
 	}
 }
--- harbinPegSTN/src/harbinPegSTN/SaveTheNetworkModel.java
+++ harbinPegSTN/src/harbinPegSTN/SaveTheNetworkModel.java
@@ -1,12 +1,13 @@
 package harbinPegSTN;
 
+import java.awt.Point;
+
 public class SaveTheNetworkModel extends Model{
 
 	
 	public SaveTheNetworkModel(){
 		super();
-		for(int i=1;i<=6;i++)
-			super.setPegAt(Peg.NONE, i);
+		//Board is NONE by default
 		setPegAt(Peg.BLACK,7);
 		setPegAt(Peg.BLACK,8);
 		setPegAt(Peg.BLACK,12);
@@ -14,5 +15,120 @@ public class SaveTheNetworkModel extends Model{
 		for(int i=14;i<=33;i++)
 			super.setPegAt(Peg.BLACK, i);
 			
+	}
+	public boolean checkMove(Point fPt,Point sPt){
+		int fLoc=this.pointToPegID(fPt.x, fPt.y);
+		int sLoc=this.pointToPegID(sPt.x, sPt.y);
+		return checkMove(fLoc,sLoc);
+	}
+	public boolean makeMove(Point fPt,Point sPt){
+		int fLoc=this.pointToPegID(fPt.x, fPt.y);
+		int sLoc=this.pointToPegID(sPt.x, sPt.y);
+		return makeMove(fLoc,sLoc);
+	}
+	public boolean makeMove(int firstLoc, int secLoc) {
+		if(!checkMove(firstLoc,secLoc))
+			return false;
+		Point mid=new Point();
+		
+		Point fPt=pegIDToPoint(firstLoc);
+		Point sPt=pegIDToPoint(secLoc);
+		switch(pegs[fPt.x][fPt.y]){
+		case BLACK:
+			pegs[fPt.x][fPt.y]=Peg.BLANK;
+			pegs[sPt.x][sPt.y]=Peg.BLACK;
+			return true;
+			
+		case WHITE:
+			//white can move either way
+			//1: move
+			if(Math.abs(fPt.x-sPt.x)<=1&& Math.abs(fPt.y-sPt.y)<=1){
+				pegs[fPt.x][fPt.y]=Peg.BLANK;
+				pegs[sPt.x][sPt.y]=Peg.WHITE;
+				return true;
+			}
+			//2: jump
+			if(Math.abs(fPt.x-sPt.x)<=2 &&Math.abs(fPt.y-sPt.y)<=2){
+				//horizontal jump
+				if(Math.abs(fPt.x-sPt.x)==2&& fPt.y==sPt.y){
+					mid.x=fPt.x>sPt.x?fPt.x-1:fPt.x+1;
+					mid.y=fPt.y;
+				}
+				// diagonal jump
+				else if(Math.abs(fPt.x-sPt.x)==2&& Math.abs(fPt.y-sPt.y)==2){
+					mid.x=fPt.x>sPt.x?fPt.x-1:fPt.x+1;
+					mid.y=fPt.y>sPt.y?fPt.y-1:fPt.y+1;
+					
+				}
+				//vertical jump
+				else if(Math.abs(fPt.y-sPt.y)==2&&fPt.x==sPt.x){
+					mid.x=fPt.x;
+					mid.y=fPt.y>sPt.y?fPt.y-1:fPt.y+1;
+				}
+				
+				pegs[fPt.x][fPt.y]=Peg.BLANK;
+				pegs[mid.x][mid.y]=Peg.BLANK;
+				pegs[sPt.x][sPt.y]=Peg.WHITE;
+				return true;
+			}
+		
+		}
+		return false;
+	}
+	public boolean checkMove(int firstLoc,int secLoc){
+		if(firstLoc<1||firstLoc>33)
+			return false;
+		if(secLoc<1||secLoc>33)
+			return false;
+		Point mid=new Point();
+		
+		Point fPt=pegIDToPoint(firstLoc);
+		Point sPt=pegIDToPoint(secLoc);
+		switch(pegs[fPt.x][fPt.y]){
+		case INVALID:
+		case NONE:
+		case BLANK:
+			return false;
+		case BLACK:
+			//black dots can only move forward and sideways
+			if((fPt.x==sPt.x && Math.abs(fPt.y-sPt.y)==1)||(fPt.x==sPt.x+1 && fPt.y==sPt.y)||(fPt.x==sPt.x+1 && Math.abs(fPt.y-sPt.y)==1)){
+				return !isPegAtLocation(secLoc);
+			}
+			return false;
+			
+		case WHITE:
+			//white can move either way
+			//1: move
+			if(Math.abs(fPt.x-sPt.x)<=1&& Math.abs(fPt.y-sPt.y)<=1){
+				return !this.isPegAtLocation(secLoc);
+			}
+			//2: jump
+			if(Math.abs(fPt.x-sPt.x)<=2 &&Math.abs(fPt.y-sPt.y)<=2){
+				//horizontal jump
+				if(Math.abs(fPt.x-sPt.x)==2&& fPt.y==sPt.y){
+					mid.x=fPt.x>sPt.x?fPt.x-1:fPt.x+1;
+					mid.y=fPt.y;
+				}
+				// diagonal jump
+				else if(Math.abs(fPt.x-sPt.x)==2&& Math.abs(fPt.y-sPt.y)==2){
+					mid.x=fPt.x>sPt.x?fPt.x-1:fPt.x+1;
+					mid.y=fPt.y>sPt.y?fPt.y-1:fPt.y+1;
+					
+				}
+				//vertical jump
+				else if(Math.abs(fPt.y-sPt.y)==2&&fPt.x==sPt.x){
+					mid.x=fPt.x;
+					mid.y=fPt.y>sPt.y?fPt.y-1:fPt.y+1;
+				}
+				else return false;
+					
+			}
+			
+			return pegs[mid.x][mid.y]==Peg.BLACK && !isPegAtLocation(secLoc);
+			
+		
+		}
+		return false;
+
 	}
 }
