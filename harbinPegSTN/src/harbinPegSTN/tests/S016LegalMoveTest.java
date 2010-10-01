29f8bbc4aa7add1a851b64e1c0cac41000ec2914
worldwise001 <worldwise001@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-01 20:28:01 +0000

    Adding some tests for S016

--- harbinPegSTN/src/harbinPegSTN/tests/S016LegalMoveTest.java
+++ harbinPegSTN/src/harbinPegSTN/tests/S016LegalMoveTest.java
@@ -35,10 +35,27 @@ public class S016LegalMoveTest {
 	}
 	
 	@Test
-	public void testLeftMove() {
-		assertTrue("Peg successfully moves from 3 to 1:", model.makeMove(3, 1));
-		assertFalse("Peg cannot move from 3 to 4:", model.makeMove(3, 4));
-		assertFalse("Peg cannot move from 8 to 6:", model.makeMove(8, 6));
+	public void testCheckMove() {
+		assertTrue("Legal move from 3 to 1", model.checkMove(3, 1));
+		assertTrue("Legal move from 3 to 11", model.checkMove(3, 11));
+		assertTrue("Legal move from 10 to 2", model.checkMove(10, 2));
+		assertTrue("Legal move from 17 to 5", model.checkMove(17, 5));
+		assertTrue("Legal move from 23 to 25", model.checkMove(23, 25));
+		assertFalse("Illegal move from 3 to 4", model.checkMove(3, 4));
+		assertFalse("Illegal move from 8 to 6", model.checkMove(8, 6));
+		assertFalse("Illegal move out of boundary 2 to 0", model.checkMove(2, 0));
+		assertFalse("Illegal move out of boundary 32 to 24", model.checkMove(32, 34));
+	}
+	
+	@Test
+	public void testPegPuzzleMove() {
+		assertTrue("Legal move from 5 to 17", model.makeMove(5, 17));
+		assertTrue("Legal move from 12 to 10", model.makeMove(12, 10));
+		assertTrue("Legal move from 26 to 12", model.makeMove(26, 12));
+		assertTrue("Legal move from 24 to 26", model.makeMove(24, 26));
+		
+		assertFalse("Illegal move from 8 to 10 (peg-to-peg)", model.makeMove(8, 10));
+		assertFalse("Illegal move from 24 to 10 (blank-to-peg)", model.makeMove(24, 10));
 	}
 
 }
