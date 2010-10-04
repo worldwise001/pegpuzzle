c24fa5b058291d049852ba9cceb29c8f22f5135e
worldwise001 <worldwise001@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-04 20:50:00 +0000

    updated old UI diagram added more comprehensive UI/story diagram/walkthrough Starting to fill in BoardPanel

--- harbinPegSTN/src/harbinPegSTN/gui/BoardPanel.java
+++ harbinPegSTN/src/harbinPegSTN/gui/BoardPanel.java
@@ -1,7 +1,30 @@
 package harbinPegSTN.gui;
 
+import harbinPegSTN.model.Model;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private Model model = null;
+	private JButton[] buttons = new JButton[33];
+	
+	public BoardPanel(Model m) {
+		model = m;
+		buildGUI();
+		updateGUI();
+	}
+
+	private void updateGUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void buildGUI() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
