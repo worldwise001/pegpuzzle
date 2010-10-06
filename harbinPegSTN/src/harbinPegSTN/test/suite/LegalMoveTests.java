212d24ade5b3b47edef3c2adc4731ac4bb591b4b
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-06 19:59:14 +0000

    create suite test for LegalMove

---
+++ harbinPegSTN/src/harbinPegSTN/test/suite/LegalMoveTests.java
@@ -0,0 +1,19 @@
+package harbinPegSTN.test.suite;
+import harbinPegSTN.test.stories.S015LegalMoveTest;
+import harbinPegSTN.test.stories.S016LegalMoveTest;
+import harbinPegSTN.test.stories.S017LegalMoveTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	S015LegalMoveTest.class,
+	S016LegalMoveTest.class,
+	S017LegalMoveTest.class
+})
+
+
+public class LegalMoveTests {
+	
+}
