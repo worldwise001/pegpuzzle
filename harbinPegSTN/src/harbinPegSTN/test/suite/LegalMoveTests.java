b2fc210d54ad8e75726889d0872ed4067eb6f1b6
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-07 20:41:23 +0000

    udpate suite test to include s110 and s115

--- harbinPegSTN/src/harbinPegSTN/test/suite/LegalMoveTests.java
+++ harbinPegSTN/src/harbinPegSTN/test/suite/LegalMoveTests.java
@@ -2,6 +2,8 @@ package harbinPegSTN.test.suite;
 import harbinPegSTN.test.stories.S015LegalMoveTest;
 import harbinPegSTN.test.stories.S016LegalMoveTest;
 import harbinPegSTN.test.stories.S017LegalMoveTest;
+import harbinPegSTN.test.stories.S110WhiteLegalMoveTest;
+import harbinPegSTN.test.stories.S115BlackLegalMoveTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,7 +12,9 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
 	S015LegalMoveTest.class,
 	S016LegalMoveTest.class,
-	S017LegalMoveTest.class
+	S017LegalMoveTest.class,
+	S110WhiteLegalMoveTest.class,
+	S115BlackLegalMoveTest.class
 })
 
 
