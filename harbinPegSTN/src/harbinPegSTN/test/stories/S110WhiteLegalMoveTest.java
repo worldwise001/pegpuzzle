bdfb5171cfa49a25b32352979030e6040fc0abfe
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-07 20:40:57 +0000

    Create test for S110 and S115

---
+++ harbinPegSTN/src/harbinPegSTN/test/stories/S110WhiteLegalMoveTest.java
@@ -0,0 +1,21 @@
+package harbinPegSTN.test.stories;
+
+import harbinPegSTN.model.SaveTheNetworkModel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class S110WhiteLegalMoveTest {
+
+	SaveTheNetworkModel model = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		model = new SaveTheNetworkModel();
+	}
+	
+	@Test
+	public void testWhiteMove(){
+		
+	}
+}
---
+++ harbinPegSTN/src/harbinPegSTN/test/stories/S115BlackLegalMoveTest.java
@@ -0,0 +1,15 @@
+package harbinPegSTN.test.stories;
+
+import harbinPegSTN.model.SaveTheNetworkModel;
+
+import org.junit.Before;
+
+public class S115BlackLegalMoveTest {
+
+	SaveTheNetworkModel model = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		model = new SaveTheNetworkModel();
+	}
+}
