263e24bd57283eb17fdc3ebef8b6efb205840579
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-08 20:02:22 +0000

    finish test cases for stories S110 and S115

--- harbinPegSTN/src/harbinPegSTN/test/stories/S110WhiteLegalMoveTest.java
+++ harbinPegSTN/src/harbinPegSTN/test/stories/S110WhiteLegalMoveTest.java
@@ -1,6 +1,8 @@
 package harbinPegSTN.test.stories;
 
+import static org.junit.Assert.*;
 import harbinPegSTN.model.SaveTheNetworkModel;
+import harbinPegSTN.model.Model.Peg;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,9 +15,18 @@ public class S110WhiteLegalMoveTest {
 	public void setUp() throws Exception {
 		model = new SaveTheNetworkModel();
 	}
-	
+	/**
+	 * white moves
+	 * jump or hop
+	 */
 	@Test
 	public void testWhiteMove(){
-		
+		model.setPegAt(Peg.WHITE, 4);
+		model.setPegAt(Peg.WHITE, 5);
+		assertTrue("check white hop from 4 to 9", model.makeMove(4, 9));
+		assertFalse("check illegal hop from 9 to 16",model.makeMove(9, 16));
+		assertFalse("check illegal jump from 9 to 23",model.makeMove(9, 23));
+		model.setPegAt(Peg.NONE, 23);
+		assertTrue("check white jump from 9 to 23",model.makeMove(9, 23));
 	}
 }
--- harbinPegSTN/src/harbinPegSTN/test/stories/S115BlackLegalMoveTest.java
+++ harbinPegSTN/src/harbinPegSTN/test/stories/S115BlackLegalMoveTest.java
@@ -1,8 +1,10 @@
 package harbinPegSTN.test.stories;
 
+import static org.junit.Assert.*;
 import harbinPegSTN.model.SaveTheNetworkModel;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class S115BlackLegalMoveTest {
 
@@ -11,5 +13,14 @@ public class S115BlackLegalMoveTest {
 	@Before
 	public void setUp() throws Exception {
 		model = new SaveTheNetworkModel();
+	}
+	
+	@Test
+	public void testBlackLegalMove(){
+		assertTrue("black legal hop from 8 to 9",model.makeMove(8, 9));
+		assertTrue("black legal hop from 9 to 4",model.makeMove(9, 4));
+		assertFalse("Illegal black backward hop from 4 to 9",model.makeMove(4, 9));
+		assertFalse("Illegal black jump from 4 to 6",model.makeMove(4, 6));
+		
 	}
 }
