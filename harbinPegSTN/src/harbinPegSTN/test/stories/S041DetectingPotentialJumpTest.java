74d3f88f06513bd33ff4c981ea641fa10045bf2d
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-08 21:12:44 +0000

    create test cases for s041

---
+++ harbinPegSTN/src/harbinPegSTN/test/stories/S041DetectingPotentialJumpTest.java
@@ -0,0 +1,29 @@
+package harbinPegSTN.test.stories;
+
+import static org.junit.Assert.*;
+import harbinPegSTN.model.SaveTheNetworkModel;
+import harbinPegSTN.model.Model.Peg;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class S041DetectingPotentialJumpTest {
+
+	//detecting potential jumps for STN Model
+SaveTheNetworkModel model = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		model = new SaveTheNetworkModel();
+	}
+	
+	@Test
+	public void testCheckJump(){
+		model.setPegAt(Peg.WHITE, 10);
+		model.setPegAt(Peg.NONE, 24);
+		assertTrue("White potential jump from 10 to 24", model.checkJump(10, 24));
+		assertTrue("White potential jump from 10 to 9", model.checkJump(10, 9));
+		assertFalse("Illegal move from 10 to 22",model.checkJump(10, 22));
+		
+	}
+}
