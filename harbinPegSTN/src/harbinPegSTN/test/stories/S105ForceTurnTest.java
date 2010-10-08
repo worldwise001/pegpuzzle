fa3cd66626cdf6463371910aea3088d815c02443
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-08 20:24:15 +0000

    Crate test case for S105 Force Turn

---
+++ harbinPegSTN/src/harbinPegSTN/test/stories/S105ForceTurnTest.java
@@ -0,0 +1,26 @@
+package harbinPegSTN.test.stories;
+
+import static org.junit.Assert.*;
+import harbinPegSTN.model.SaveTheNetworkModel;
+import harbinPegSTN.model.Model.Peg;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class S105ForceTurnTest {
+SaveTheNetworkModel model = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		model = new SaveTheNetworkModel();
+	}
+	
+	@Test
+	public void testForceTurn(){
+		model.setPegAt(Peg.WHITE, 4);
+		assertFalse("Start with white's turn, so all black move is illegal, try black move from 8 to 9", model.makeMove(8, 9));
+		assertTrue("White legal move from 4 to 9", model.makeMove(4, 9));
+		model.reverseTurn();
+		assertTrue("Now black's turn, legal move from 12 to 11",model.makeMove(12, 11));
+	}
+}
