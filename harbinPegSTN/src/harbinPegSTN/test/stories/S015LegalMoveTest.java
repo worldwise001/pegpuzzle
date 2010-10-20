d5d985b45e6d72f17448f3c934de4aa06ef1d8d3
ding <ding@58946348-bbae-4801-9ad5-a2221e735b4c>
2010-10-20 20:21:58 +0000

    add test cases

--- harbinPegSTN/src/harbinPegSTN/test/stories/S015LegalMoveTest.java
+++ harbinPegSTN/src/harbinPegSTN/test/stories/S015LegalMoveTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import java.awt.Point;
 
 import harbinPegSTN.model.Model;
+import harbinPegSTN.model.Peg;
 import harbinPegSTN.model.Status;
 
 import org.junit.Before;
@@ -57,6 +58,36 @@ public class S015LegalMoveTest {
 		assertEquals("Should be invalid in the begining",Status.INVALID,model.getStatus());
 		model.setStatus(Status.WHITE_CLICK);
 		assertEquals("Should be white click",Status.WHITE_CLICK,model.getStatus());
+		
+	}
+	@Test 
+	public void testTogglePeg(){
+		assertFalse("no selection made yet, so toggle return false",model.togglePeg(0));
+		assertTrue("now toggle peg at 19", model.togglePeg(19));
+		assertFalse("now toggle peg at 17, should be false because not jump possible for base model",model.togglePeg(17));
+		assertEquals("selected peg should be 19 now",19,model.getSelectedPeg());
+		
+	}
+	@Test
+	public void testIsPegAtLocation(){
+		assertFalse("no peg yet",model.isPegAtLocation(17));
+		model.setPeg(Peg.NORMAL, 17);
+		assertTrue("have a peg at location 17",model.isPegAtLocation(17));
+	}
+	
+	@Test 
+	public void testStatusToInt(){
+		assertEquals("true",3,Status.toInt(Status.WHITE_SLIDE));
+		assertEquals("true",10,Status.toInt(Status.INVALID));
+		assertEquals("true",7,Status.toInt(Status.BLACK_CLICK));
+
+		assertEquals("true",1,Status.toInt(Status.WHITE_CLICK));
+		assertEquals("true",2,Status.toInt(Status.WHITE_JUMP));
+		assertEquals("true",5,Status.toInt(Status.WHITE_TURN));
+		
+
+		assertEquals("true",4,Status.toInt(Status.WHITE_REMOVED));
+		assertEquals("true",6,Status.toInt(Status.BLACK_TURN));
 		
 	}
 }
